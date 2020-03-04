6499754d1b128fc9e001663dcc354e1cad8c440b
Kourosh <mousavi.card@gmail.com>
2020-03-04 14:48:50 +0330

    fix serilize bug

--- baseapp/src/main/java/com/parvanpajooh/baseapp/models/UpdateModel.kt
+++ baseapp/src/main/java/com/parvanpajooh/baseapp/models/UpdateModel.kt
@@ -1,9 +1,16 @@
 package com.parvanpajooh.baseapp.models
 
+import com.google.gson.annotations.SerializedName
+
 data class UpdateModel (
+        @SerializedName("latestVersion")
         val latestVersion: String,
+        @SerializedName("latestVersionCode")
         val latestVersionCode: Int,
+        @SerializedName("url")
         val url: String,
+        @SerializedName("required")
         val required: Boolean,
+        @SerializedName("releaseNotes")
         val releaseNotes: List<String>
     )

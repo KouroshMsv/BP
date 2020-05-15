ed3933a34dddeb892a2d43f48583402dcda767b4
Koursoh <mousavi.card@gmail.com>
2020-05-15 20:53:50 +0430

    fix bug

--- Sample/src/main/java/com/parvanpajooh/baseparvan/Api.kt
+++ Sample/src/main/java/com/parvanpajooh/baseparvan/Api.kt
@@ -4,7 +4,7 @@ import com.parvanpajooh.basedata.BaseDataModuleRepositoryImpl
 import com.parvanpajooh.basedata.net.BaseRestApiImpl
 import dev.kourosh.basedomain.Result
 
-class Api() : BaseDataModuleRepositoryImpl(BaseRestApiImpl("https://172.24.24.38")) {
+class Api : BaseDataModuleRepositoryImpl(BaseRestApiImpl("https://172.24.24.38", false)) {
     override suspend fun initialize(token: String): Result<Unit> {
         return Result.Success(Unit)
 

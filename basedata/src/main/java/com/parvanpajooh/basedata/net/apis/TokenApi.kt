f5f8f44ce0b331166912c66accd48fb1eb64e9d7
Kourosh <mousavi.card@gmail.com>
2020-10-14 11:36:03 +0330

    fix token api bug

--- basedata/src/main/java/com/parvanpajooh/basedata/net/apis/TokenApi.kt
+++ basedata/src/main/java/com/parvanpajooh/basedata/net/apis/TokenApi.kt
@@ -2,26 +2,23 @@ package com.parvanpajooh.basedata.net.apis
 
 import android.util.Base64
 import com.parvanpajooh.basedata.net.models.TokenRes
-import kotlinx.coroutines.Deferred
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
 open interface TokenApi {
-	private val tokenAuthorization: String
-		get() = ("Basic "+Base64.encodeToString("android:androidclientsecret".toByteArray(), Base64.DEFAULT)).trim { it <= ' ' }
+    private val tokenAuthorization: String
+        get() = ("Basic " + Base64.encodeToString("android:androidclientsecret".toByteArray(), Base64.DEFAULT)).trim { it <= ' ' }
 
-	@FormUrlEncoded
-	@POST("/uaa/oauth/token")
-	@Headers("Authorization: Basic YW5kcm9pZDphbmRyb2lkY2xpZW50c2VjcmV0")
-	fun getTokenWithAccountAsync(
-			@Field("username") username: String, @Field("password") password: String, @Field("grant_type") grantType: String = "password"): TokenRes
+    @FormUrlEncoded
+    @POST("/uaa/oauth/token")
+    @Headers("Authorization: Basic YW5kcm9pZDphbmRyb2lkY2xpZW50c2VjcmV0")
+    suspend fun getTokenWithAccountAsync(@Field("username") username: String, @Field("password") password: String, @Field("grant_type") grantType: String = "password"): TokenRes
 
-	@FormUrlEncoded
-	@POST("/uaa/oauth/token")
-	@Headers("Authorization: Basic YW5kcm9pZDphbmRyb2lkY2xpZW50c2VjcmV0")
-	fun getTokenWithRefreshTokenAsync(
-			@Field("refresh_token") refreshToken: String, @Field("grant_type") grantType: String = "refresh_token"): TokenRes
+    @FormUrlEncoded
+    @POST("/uaa/oauth/token")
+    @Headers("Authorization: Basic YW5kcm9pZDphbmRyb2lkY2xpZW50c2VjcmV0")
+    suspend fun getTokenWithRefreshTokenAsync(@Field("refresh_token") refreshToken: String, @Field("grant_type") grantType: String = "refresh_token"): TokenRes
 
 }
